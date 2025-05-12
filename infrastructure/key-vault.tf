5da09e6ccac94a7ec4a66d08bda9a9f4731a6111
mircea.talu <talumircea13@gmail.com>
2025-05-12 13:43:20 +0300

    Fix key vault name not unique

--- infrastructure/key-vault.tf
+++ infrastructure/key-vault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "main" {
-  name                        = "kv-${var.project}-${var.environment}"
+  name                        = "kv-mgmt-${var.location}-${var.environment}"
   location                    = azurerm_resource_group.main.location
   resource_group_name         = azurerm_resource_group.main.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
